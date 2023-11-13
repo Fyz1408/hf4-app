f1efea83299baa14a9555fdb8ad7c52554c675df
Fyz1408 <daniellykke1@gmail.com>
2023-11-13 10:20:25 +0100

    Initial commit

---
+++ hf4-app/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace hf4_app;
+
+public partial class AppShell : Shell
+{
+  public AppShell()
+  {
+    InitializeComponent();
+  }
+}
+
---
+++ hf4-app/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace hf4_app;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+  protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
