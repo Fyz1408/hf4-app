49289babf172ab4c4fd5b63e20169f412400a095
jacob <jacobspil@hotmail.dk>
2023-11-15 09:39:30 +0100

    så er der den fister exmpel af https get

---
+++ hf4-app/service/webHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hf4_app.service
+{
+    class webHandler
+    {
+        private string baseUrl = "https://skol.molsen.one";
+        private string token;
+
+        static readonly HttpClient client = new HttpClient();
+
+        private async Task get(string path)
+        {
+            HttpResponseMessage response = await client.GetAsync(baseUrl+path);
+
+        }
+    }
+}
