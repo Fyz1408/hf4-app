45681af6aee74336499db0d9f587302c6e490c0c
jacob <jacobspil@hotmail.dk>
2023-11-15 12:29:03 +0100

    så vikker post også

--- hf4-app/service/webHandler.cs
+++ hf4-app/service/webHandler.cs
@@ -8,15 +8,29 @@ namespace hf4_app.service
 {
     class webHandler
     {
-        private string baseUrl = "https://skol.molsen.one";
+        private string baseUrl = "https://skol.molsen.one/";
         private string token;
 
         static readonly HttpClient client = new HttpClient();
 
-        private async Task get(string path)
-        {
-            HttpResponseMessage response = await client.GetAsync(baseUrl+path);
 
+        private async Task<string> get(string path)
+        {
+            HttpResponseMessage response = await client.GetAsync(baseUrl);
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+            return null;
+        }
+        private async Task<string> post(string path,string data)
+        {
+            HttpResponseMessage response = await client.PostAsync(baseUrl + path, new ByteArrayContent(Encoding.ASCII.GetBytes("data")));
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+            return null;
         }
     }
 }
