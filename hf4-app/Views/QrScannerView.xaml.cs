ac7198a37c187f23e3f73182554939faa8a7b366
Fyz1408 <daniellykke1@gmail.com>
2023-11-16 11:45:28 +0100

    Added lots of styling to the QrScannerView

--- hf4-app/Views/QrScannerView.xaml.cs
+++ hf4-app/Views/QrScannerView.xaml.cs
@@ -9,29 +9,5 @@ public partial class QrScannerView : ContentPage
   public QrScannerView()
   {
     InitializeComponent();
-    CameraView.CamerasLoaded += CameraView_CamerasLoaded;
-  }
-
-  private void CameraView_CamerasLoaded(object sender, EventArgs e)
-  {
-    if (CameraView.Cameras.Count > 0)
-    {
-      CameraView.Camera = CameraView.Cameras.First();
-      MainThread.BeginInvokeOnMainThread(async () =>
-      {
-        CameraView.MirroredImage = true;
-        await CameraView.StopCameraAsync();
-        await CameraView.StartCameraAsync();
-      });
-    }
-  }
-
-  private void CameraView_OnBarcodeDetected(object sender, BarcodeEventArgs args)
-  {
-    // Barcode scanning happens on a background thread therefor we need to invoke the main thread to update the UI 
-    MainThread.BeginInvokeOnMainThread(() =>
-    {
-      BarcodeResult.Text = $"{args.Result[0].BarcodeFormat}: {args.Result[0].Text}";
-    });
   }
 }
