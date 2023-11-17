25702a347cc74ed70bd8e7fa231cc42b7f66653b
Fyz1408 <daniellykke1@gmail.com>
2023-11-17 09:20:28 +0100

    Added some styling and test data for the packageView

--- hf4-app/ViewModel/PackageViewModel.cs
+++ hf4-app/ViewModel/PackageViewModel.cs
@@ -1,5 +1,7 @@
+using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using hf4_app.Models;
 
 namespace hf4_app.ViewModel;
 
@@ -9,9 +11,53 @@ public partial class PackageViewModel : ObservableObject
 {
   [ObservableProperty] private string package;
 
+  // Test data is temporary until api is integrated 
+  [ObservableProperty] private ObservableCollection<PackageModel.PackageEvent> packageEvents = new()
+  {
+    new PackageModel.PackageEvent()
+    {
+      Id = 1,
+      WarehouseId = 1,
+      Timestamp = DateTime.UtcNow
+    },
+    new PackageModel.PackageEvent()
+    {
+      Id = 2,
+      WarehouseId = 2,
+      Timestamp = DateTime.UtcNow
+    },
+    new PackageModel.PackageEvent()
+    {
+      Id = 3,
+      WarehouseId = 5,
+      Timestamp = DateTime.UtcNow
+    },
+    new PackageModel.PackageEvent()
+    {
+      Id = 4,
+      WarehouseId = 5,
+      Timestamp = DateTime.UtcNow
+    },
+    new PackageModel.PackageEvent()
+    {
+      Id = 5,
+      WarehouseId = 6,
+      Timestamp = DateTime.UtcNow
+    },
+    new PackageModel.PackageEvent()
+    {
+      Id = 6,
+      WarehouseId = 7,
+      Timestamp = DateTime.UtcNow
+    }
+  };
+  
+
+
   [RelayCommand]
   private static async Task GoBack()
   {
+    // Return to QR scanner page
     await Shell.Current.GoToAsync("..");
   }
 }
